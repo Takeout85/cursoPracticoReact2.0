cd2512a95ed3298a700c0a8c4fea76f231759558
Takeout <t4k30ut85@gmail.com>
2021-10-15 12:19:13 -0500

    Context agregado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
 import { Menu } from '@components/Menu';
+import { AppContext } from '@context/AppContext'
 
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
@@ -8,6 +9,7 @@ import ShoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
   const [toggle ,setToggle] = useState(false);
+  const { state } = useContext(AppContext)
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -46,7 +48,7 @@ const Header = () => {
           </li>
 					<li className="navbar-shopping-cart">
 						<img src={ShoppingCart} alt="shopping cart" />
-						<div>2</div>
+						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
 					</li>
 				</ul>
 			</div>
