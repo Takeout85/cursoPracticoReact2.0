b94e6499b80213924a58b8df3ee45e0ce3356a88
Takeout <t4k30ut85@gmail.com>
2021-10-15 15:45:41 -0500

    [ADD] GitHub Actions

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import close from '@icons/icon_close.png'
+import React, { useContext } from 'react';
+import close from '@icons/icon_close.png';
+import { AppContext } from '@context/AppContext';
 import '@styles/OrderItem.scss';
 
-const OrderItem = () => {
+const OrderItem = ({ product }) => {
+  const { removeFromCart } = useContext(AppContext);
 	return (
 		<div className="OrderItem">
 			<figure>
-				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+				<img src={product.images[0]} alt={product.title} />
 			</figure>
-			<p>Bike</p>
-			<p>$30,00</p>
-			<img src={close} alt="close" />
+			<p>{product.title}</p>
+			<p>${product.price}</p>
+			<img src={close} alt="close" onClick={() => removeFromCart(product)}/>
 		</div>
 	);
 }
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
-import flechit from '@icons/flechita.svg'
-import OrderItem from '@components/OrderItem';
+import React, { useContext } from 'react';
 import '@styles/MyOrder.scss'
+import { OrderItem } from '@components/OrderItem';
+import { AppContext } from '@context/AppContext';
+
+import arrow from '@icons/flechita.svg'
 
 const MyOrder = () => {
+  const { state } = useContext(AppContext);
+
+  const sumTotal = () => {
+    const reducer = (accumulator, currenValue) => accumulator + currenValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  }
+
   return (
     <aside className="MyOrder">
 			<div className="title-container">
-				<img src={flechit} alt="arrow" />
+				<img src={arrow} alt="arrow" />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map(product => (
+          <OrderItem product={product} key={`orderItem-${product.id}`}/>
+        ))}
 				<div className="order">
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
